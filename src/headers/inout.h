19875e8f55e5a12af7470d4dfa2de327f0d13b05
tkanigara <19623038@std.stei.itb.ac.id>
2025-04-10 22:45:46 +0700

    refactor: delete unused functions

--- src/headers/inout.h
+++ src/headers/inout.h
@@ -17,15 +17,9 @@ struct Node {
 
 std::vector<std::vector<RGB>> loadImage(const std::string& filename, int& width, int& height);
 
-void fillImageUntilDepth(std::vector<std::vector<RGB>>& img, const Node* node, int currentDepth, int maxDepth);
-
-void saveStepImage(const std::vector<std::vector<RGB>>& img, const std::string& folder = "steps");
-
-
 void fillImage(std::vector<std::vector<RGB>> &img, const Node* node);
 
 void saveImage(const std::vector<std::vector<RGB>>& img, const std::string& filename);
-
 
 void fillImageWithDepthLimit(std::vector<std::vector<RGB>>& image, Node* node, int maxDepth, int currentDepth = 0);
 
